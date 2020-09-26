26916704f8a5feef61013f42f75c95f51a1f679b
syawal2kalii <syawal20162016@gmail.com>
2020-09-26 22:04:12 +0800

    add model and migration

---
+++ algen/penjadwalan/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Dosen(models.Model):
+    nama_dosen = models.CharField(max_length=100)
+    id_dosen = models.CharField(default='', max_length=100)
+    no_hp = models.CharField(default='', max_length=100)
+
+
+class Asisten(models.Model):
+    nama_asisten = models.CharField(max_length=100, default='')
+    no_hp = models.CharField(default='', max_length=100)
+    nim = models.CharField(default='', max_length=100)
+
+
+class Mata_praktikum(models.Model):
+    nama_praktikum = models.CharField(default='', max_length=100)
+    kode_praktikum = models.CharField(default='', max_length=100)
+
+
+class Hari(models.Model):
+    nama_hari = models.CharField(default='', max_length=100)
+
+class Jam(models.Model):
+    keterangan = models.CharField(default='', max_length=100)
+
+class Ruangan(models.Model):
+    nama_ruangan = models.CharField(default='', max_length=100)
