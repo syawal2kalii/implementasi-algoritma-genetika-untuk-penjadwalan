383db4570a5d36cd11c25ccd4423bafb5957acb7
syawal2kalii <syawal20162016@gmail.com>
2020-09-28 14:54:14 +0800

    add django admin

--- algen/penjadwalan/admin.py
+++ algen/penjadwalan/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Asisten, Dosen, Hari, Jam, Ruangan, Mata_praktikum
 
 # Register your models here.
+admin.site.register(Asisten)
+admin.site.register(Dosen)
+admin.site.register(Hari)
+admin.site.register(Jam)
+admin.site.register(Ruangan)
+admin.site.register(Mata_praktikum)
--- algen/penjadwalan/models.py
+++ algen/penjadwalan/models.py
@@ -7,23 +7,43 @@ class Dosen(models.Model):
     id_dosen = models.CharField(default='', max_length=100)
     no_hp = models.CharField(default='', max_length=100)
 
+    def __str__(self):
+        return self.nama_dosen
 
 class Asisten(models.Model):
     nama_asisten = models.CharField(max_length=100, default='')
     no_hp = models.CharField(default='', max_length=100)
     nim = models.CharField(default='', max_length=100)
 
+    def __str__(self):
+        return self.nama_asisten
+    
+
 
 class Mata_praktikum(models.Model):
     nama_praktikum = models.CharField(default='', max_length=100)
     kode_praktikum = models.CharField(default='', max_length=100)
 
-
+    def __str__(self):
+        return self.nama_praktikum
+    
 class Hari(models.Model):
     nama_hari = models.CharField(default='', max_length=100)
+
+    def __str__(self):
+        return self.nama_hari
+    
 
 class Jam(models.Model):
     keterangan = models.CharField(default='', max_length=100)
 
+    def __str__(self):
+        return self.keterangan
+    
+
 class Ruangan(models.Model):
     nama_ruangan = models.CharField(default='', max_length=100)
+
+    def __str__(self):
+        return self.nama_ruangan
+    
